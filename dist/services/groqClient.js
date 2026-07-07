370975e3bccdd8120ccd2b1d572c4cefab5142e9
AMAL <amgt503@gmail.com>
2026-07-07 19:18:41 +0530

    Remove dist from git tracking (gitignored)

--- dist/services/groqClient.js
+++
@@ -1,4 +0,0 @@
-import Groq from "groq-sdk";
-import { env, isGroqConfigured } from "../config/env.js";
-export const groqClient = isGroqConfigured ? new Groq({ apiKey: env.GROQ_API_KEY }) : null;
-//# sourceMappingURL=groqClient.js.map
